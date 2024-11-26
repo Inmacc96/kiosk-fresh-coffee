d419e41d2885984e5d3d0cce96bbdd9dac13d3a5
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-26 20:15:59 +0100

    retrieve categories from db

--- app/ui/order/OrderSidebar.tsx
+++ app/ui/order/OrderSidebar.tsx
@@ -1,4 +1,11 @@
-const OrderSidebar = () => {
+import { prisma } from "@/app/lib/prisma";
+async function getCategories() {
+  return await prisma.category.findMany();
+}
+
+const OrderSidebar = async () => {
+  const categories = await getCategories();
+
   return <aside className="md:w-72 md:h-screen bg-white">OrderSide bar</aside>;
 };
 
