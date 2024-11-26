ca885178fc92fb84ca1f90302d01c3c1fc6c6dda
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-26 20:50:54 +0100

    get products by category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,9 +1,23 @@
+import { prisma } from "@/app/lib/prisma";
+
+const getProducts = async (category: string) => {
+  return await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+};
+
 type OrderPageProps = {
   params: Promise<{ category: string }>;
 };
 
 const OrderPage: React.FC<OrderPageProps> = async ({ params }) => {
-  console.log(await params);
+  const { category } = await params;
+  const products = await getProducts(category);
+
   return <div>OrderPage</div>;
 };
 
