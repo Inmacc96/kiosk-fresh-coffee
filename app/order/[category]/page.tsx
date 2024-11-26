ceafbe8c6e607aaa73b43efc218cb3846f6d4c6d
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-26 21:12:13 +0100

    render and style products

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/app/lib/prisma";
+import ProductCard from "@/app/ui/products/ProductCard";
 
 const getProducts = async (category: string) => {
   return await prisma.product.findMany({
@@ -18,7 +19,19 @@ const OrderPage: React.FC<OrderPageProps> = async ({ params }) => {
   const { category } = await params;
   const products = await getProducts(category);
 
-  return <div>OrderPage</div>;
+  return (
+    <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default OrderPage;
---
+++ app/ui/products/ProductCard.tsx
@@ -0,0 +1,35 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import { formatCurrency } from "../../../utils/index";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  return (
+    <div className="border bg-white rounded-lg">
+      <Image
+        width={500}
+        height={500}
+        src={`/products/${product.image}.jpg`}
+        alt={`producto ${product.name}`}
+        className="rounded-t-lg"
+      />
+      <div className="p-5">
+        <h3 className="text-2xl font-bold">{product.name}</h3>
+        <p className="mt-5 font-black text-amber-500 text-4xl">
+          {formatCurrency(product.price)}
+        </p>
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer rounded-lg"
+        >
+          Añadir
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
