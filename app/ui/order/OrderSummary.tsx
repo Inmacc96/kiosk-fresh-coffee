c251ada59952b262a195ecc1724b6d5c738afe4c
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-27 21:15:29 +0100

    render orderProductItem

--- app/ui/order/OrderSummary.tsx
+++ app/ui/order/OrderSummary.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useStore } from "@/src/store";
+import ProductDetails from "./OrderProductItem";
 
 const OrderSummary = () => {
   const order = useStore((state) => state.order);
@@ -11,7 +12,9 @@ const OrderSummary = () => {
         <p className="text-center my-10">El carrito está vacío</p>
       ) : (
         <div className="mt-5">
-          <p>Si hay algo</p>
+          {order.map((item) => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
         </div>
       )}
     </aside>
