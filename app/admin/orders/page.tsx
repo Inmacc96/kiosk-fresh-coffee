601716332f276fe3670e1edf558fcebeba266db3
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-28 20:13:24 +0100

    retrieve pending orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,23 @@
+import { prisma } from "@/app/lib/prisma";
 import Heading from "@/app/ui/Heading";
 
-const OrdersPage = () => {
+const getPendingOrders = async () => {
+  return await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+};
+
+const OrdersPage = async () => {
+  const orders = await getPendingOrders();
   return (
     <>
       <Heading>Administrar Órdenes</Heading>
