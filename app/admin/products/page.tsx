7f35a816e29e74c4b96ae65afd2496cc3655cd90
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-28 20:09:02 +0100

    layout sidebar for admin page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/app/ui/Heading";
+
+const ProductPage = () => {
+  return (
+    <>
+      <Heading>Administrar Productos</Heading>
+    </>
+  );
+};
+
+export default ProductPage;
