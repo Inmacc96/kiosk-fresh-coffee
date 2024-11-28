23a27dbe7ce6739f9e99599b0519f443fa122c4f
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-28 17:05:00 +0100

    added validation in server side

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,12 @@
 "use server";
 
-export const createOrder = async () => {
-  console.log("create order");
+import { OrderSchema } from "./schema";
+
+export const createOrder = async (data: unknown) => {
+  const result = OrderSchema.safeParse(data);
+  if (!result.success) {
+    return {
+      errors: result.error.issues,
+    };
+  }
 };
