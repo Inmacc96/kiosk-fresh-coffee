418698da790cbfe507ae097e74b3fa4ebc420cf4
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-28 16:31:15 +0100

    created createOrder server action and integrated it into OrderSummary component with a form to handle order creation

--- app/ui/order/OrderSummary.tsx
+++ app/ui/order/OrderSummary.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useMemo } from "react";
-import ProductDetails from "./OrderProductItem";
+import { createOrder } from "@/app/lib/actions";
 import { useStore } from "@/app/lib/store";
 import { formatCurrency } from "@/app/lib/utils";
+import { useMemo } from "react";
+import ProductDetails from "./OrderProductItem";
 
 const OrderSummary = () => {
   const order = useStore((state) => state.order);
@@ -11,12 +12,17 @@ const OrderSummary = () => {
     [order]
   );
 
+  const handleCreateOrder = (formData: FormData) => {
+    const name = formData.get("name");
+    createOrder();
+  };
+
   return (
     <aside className="lg:h-screen lg:overflow-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
       {order.length === 0 ? (
-        <p className="text-center my-10">El carrito está vacío</p>
+        <p className="text-center my-10">El pedido está vacío</p>
       ) : (
         <div className="mt-5">
           {order.map((item) => (
@@ -26,6 +32,20 @@ const OrderSummary = () => {
             Total a pagar: {""}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form className="w-full mt-10 space-y-5" action={handleCreateOrder}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Tu Nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+            />
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              value="Confirmar Pedido"
+            />
+          </form>
         </div>
       )}
     </aside>
