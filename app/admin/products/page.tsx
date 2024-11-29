8a966c9cced91f4885923703585a1a90408ef5b3
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-29 18:40:37 +0100

    get totalProducts count in parallel

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,6 +2,10 @@ import { prisma } from "@/app/lib/prisma";
 import Heading from "@/app/ui/Heading";
 import ProductTable from "@/app/ui/products/ProductTable";
 
+const productCount = async () => {
+  return await prisma.product.count();
+};
+
 const getProducts = async (page: number, pageSize: number) => {
   const skip = (page - 1) * pageSize;
 
@@ -23,8 +27,12 @@ type ProductPageProps = {
 const ProductPage: React.FC<ProductPageProps> = async ({ searchParams }) => {
   const page = +searchParams.page || 1;
   const pageSize = 10;
-
-  const products = await getProducts(page, pageSize);
+  const productsData = getProducts(page, pageSize);
+  const totalProductsData = productCount();
+  const [products, totalProducts] = await Promise.all([
+    productsData,
+    totalProductsData,
+  ]);
 
   return (
     <>
