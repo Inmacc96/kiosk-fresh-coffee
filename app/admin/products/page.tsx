0b21fb41cec4573533be75cabc13c037dccee3e2
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-11-29 18:10:29 +0100

    retrive and render products in admin page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,23 @@
+import { prisma } from "@/app/lib/prisma";
 import Heading from "@/app/ui/Heading";
+import ProductTable from "@/app/ui/products/ProductTable";
 
-const ProductPage = () => {
+const getProducts = async () => {
+  return await prisma.product.findMany({
+    include: {
+      category: true,
+    },
+  });
+};
+
+const ProductPage = async () => {
+  const products = await getProducts();
+
   return (
     <>
       <Heading>Administrar Productos</Heading>
+
+      <ProductTable products={products} />
     </>
   );
 };
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,5 +2,5 @@ export const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat("es-ES", {
     style: "currency",
     currency: "EUR",
-  }).format(amount);
+  }).format(amount / 10);
 };
