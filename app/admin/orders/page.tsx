98924fc32239bbf75db8dc47034c484cec130eae
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2024-12-06 20:47:06 +0100

    add "real time" when create a order

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,30 +1,33 @@
-import { prisma } from "@/app/lib/prisma";
+"use client";
+import { OrderWithProducts } from "@/app/lib/types";
 import Heading from "@/app/ui/Heading";
 import OrderCard from "@/app/ui/order/OrderCard";
+import Spinner from "@/app/ui/Spinner";
+import useSWR from "swr";
 
-const getPendingOrders = async () => {
-  return await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
+const OrdersPage = () => {
+  const url = "/admin/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+  const { data, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false,
   });
-};
-
-const OrdersPage = async () => {
-  const orders = await getPendingOrders();
+  if (isLoading)
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  if (!data) return;
   return (
     <>
       <Heading>Administrar Pedidos</Heading>
-      {orders.length ? (
+      {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
+          {data.map((order) => (
             <OrderCard key={order.id} order={order} />
           ))}
         </div>
